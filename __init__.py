b3d3c9d5e29304c49f61b486b2ca93f19a2e6b1e
Phoebe Heart <phoebe.chew@connect.qut.edu.au>
2022-06-09 16:06:00 +1000

    login change

--- __init__.py
+++ __init__.py
@@ -32,7 +32,7 @@ def create_app():
     # create a user loader function takes userid and returns User
     from .models import User  # importing here to avoid circular references
     @login_manager.user_loader
-        def load_user(user_id):
+    def load_user(user_id):
         return website.query.get(int(user_id))
 
     # importing views module here to avoid circular references
