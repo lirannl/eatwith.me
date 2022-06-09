0f9375cb2daa3926ec9869a06c368a10beef7f80
Phoebe Heart <phoebe.chew@connect.qut.edu.au>
2022-06-09 15:50:10 +1000

    user

--- website/models.py
+++ website/models.py
@@ -25,12 +25,12 @@ class website: # use the keyword class
         str.format(self.name)
         return str
 
-class Admin(User): # derived class of User1
-    def __init__(self, name, email, privilege):
-        super().__init__(name,email) #when you call base class method super()
-        self.type='admin'
-        self.privilege=privilege
-        
+#class Admin(User): # derived class of User1
+#    def __init__(self, name, email, privilege):
+#        super().__init__(name,email) #when you call base class method super()
+#        self.type='admin'
+#        self.privilege=privilege
+
 class Cuisine(db.Model):
     __tablename__ = 'tbl_cuisine'
     id          = db.Column(db.Integer, primary_key=True)
@@ -47,16 +47,14 @@ class Cuisine(db.Model):
  
     def __repr__(self): 
         return "<Name: {}>".format(self.name)
-
         
-        
-#class Comment(db.Model):
-#    __tablename__ = 'comments'
-#    id = db.Column(db.Integer, primary_key=True)
-#    text = db.Column(db.DateTime, default=datetime.now())
-#    #add the foreign keys
-#    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-#    website_id = db.Column(db.Interger, db.ForeignKey('website.id'))
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.DateTime, default=datetime.now())
+    #add the foreign keys
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    website_id = db.Column(db.Interger, db.ForeignKey('website.id'))
 
 #    def __repr__(self):
 #        return "<Comment: {}>".format(self.text)
@@ -73,39 +71,14 @@ class Cuisine(db.Model):
 
 
 #create a base class
-normal_user = User('countvoncount', 'cvc@sstreet.com')
-normal_user.set_password('vatesdenoovdeday')
+user = User('countvoncount', 'cvc@sstreet.com')
+user.set_password('vatesdenoovdeday')
 
 # create the derived or admin class
-admin_user = Admin('kermit', 'kermie@x.sstreet.com', 'xyzere')
+user = User('kermit', 'kermie@x.sstreet.com', 'xyzere')
 
  # the base class method is accessible in the derived class
-admin_user.set_password('dreamersandme') 
+user.set_password('dreamersandme') 
 
 #print both the classes
-print(normal_user)
-print(admin_user)
-
-
-
-
-
-
-
-
-#create instance of the Hotel class: __init__ method is called here
-#hotel_in_brisbane = Hotel('brisbane', 'Description of Brisbane')
-#print(hotel_in_brisbane)
-
-#create instance of the class
-#hotel_in_sydney = Hotel('sydney', 'Close to Opera house')
-#hotel_in_sydney.add_room('standard','basic room', 45,200)
-#print(hotel_in_sydney)
-#
-#hotel_in_brisbane.add_room('standard',' basic room in Brisbane', 30, 120)
-#print(hotel_in_brisbane)
-
-#you could create independent room objects but current signature of hotel
-# does not let you pass the child object, it takes control of the child object
-#std_room1 = Room('standard', 'Basic room', 30,110)
-#std_room2 = Room('standard', 'Basic room', 45,200)
+print(user)
