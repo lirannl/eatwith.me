9ebe84f8fead03581924db3f53fa9f387d4d58b1
Liran Piade <liranpiade@gmail.com>
2022-06-09 10:49:34 +0000

    Fixed book event view, server now gets event from form

--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, redirect
+from base64 import b32encode
+from flask import Blueprint, redirect, request
 from flask import render_template
 from sqlalchemy.orm import Session
 from .models import Event, User
@@ -11,9 +12,9 @@ def index():
 #Booking an event code
 @bp.route("/book_event")
 def book_event():
-    form_data = request.body
+    form_data = request.form
     user: User = None # Code for figuring out which user you're logged in as here
-    event: Event = Event.query.where(Event.id == form_data.id).first()
+    event: Event = Event.query.where(Event.id == b32encode(form_data["id"])).first()
     if (event.hostId == user.id): return 403 # However you return a 403 response because you can't book your own event
     event.attendees.append([user] * form_data.count)
     return redirect("Where?")
@@ -21,7 +22,7 @@ def book_event():
 #Connecting create event
 @bp.route("/create_event")
 def create_event():
-    form_data = request.body
+    form_data = request.form
 #     user: User = None
 
 
