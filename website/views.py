dd198a46b12cf44119440547ab45cd6f82b36df7
ChrisPham12 <105838041+ChrisPham12@users.noreply.github.com>
2022-06-09 13:59:19 +1000

    Update views.py

--- website/views.py
+++ website/views.py
@@ -1,8 +1,22 @@
 from flask import Blueprint
+from flask import render_template
+from .models import db
+from .models import Cuisine
+import datetime as DT
 
 bp = Blueprint('main', __name__)
 
 
 @bp.route('/')
 def index():
-    return '<h1>Starter code for the assessment<h1>'
+    cuisines    = Cuisine.query.all()
+    cs          = list()
+    for cuisine in cuisines:
+        if DT.datetime.now() < cuisine.date_from:
+            cuisine.status = 'Upcoming'
+        elif DT.datetime.now() > cuisine.date_from and DT.datetime.now() < cuisine.date_to:
+            cuisine.status = 'Active'
+        else:
+            cuisine.status = 'Inactive'
+        cs.append(cuisine)
+    return render_template('index.html', cuisines=cs)
