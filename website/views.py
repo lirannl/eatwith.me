96ad112e28d90123e59dc9ce5067bc64bc496f8e
Liran Piade <liranpiade@gmail.com>
2022-06-10 05:07:58 +0000

    Made a registration form

--- website/views.py
+++ website/views.py
@@ -1,27 +1,32 @@
+from crypt import methods
+from typing import Optional
+import website
 from base64 import b32encode
 from flask import Blueprint, redirect, request, url_for
 from flask import render_template
-from sqlalchemy.orm import Session
-from website.forms import LoginForm
+
+from website.forms import RegisterForm
 from .models import Event, User
 
 
 bp = Blueprint('website', __name__)
 
-import website
 
 @bp.route('/')
 def index():
-    return render_template('base.html', loginform=LoginForm())
-
+    return render_template('base.html')
 
 @bp.route('/login', methods=["POST"])
 def login():
-    print(request.values["email"])
-    return redirect()
+    username = request.values["username"]
+    password = request.values["password"]
+    user: Optional[User] = User.query.where(User.username == username).first()
+    return redirect(request.referrer)
 
 # Booking an event code
-
+@bp.route("/register", methods=["GET","POST"])
+def register():
+    return render_template("register.html", form=RegisterForm())
 
 @bp.route("/book_event")
 def book_event():
@@ -42,6 +47,7 @@ def create_event():
     form_data = request.form
 #     user: User = None
 
+
 @bp.route('/<id>')
 def show(id):
     website = website.query.get(id=id).first()
@@ -50,23 +56,24 @@ def show(id):
     return render_template('website/show.html', website=website, cform=cform)
 
 
-@bp.route('/new-event', methods = ['GET', 'POST'])
+@bp.route('/new-event', methods=['GET', 'POST'])
 def create():
-        print('Method types', request.method)
-        return redirect(url_for('models.create'))
-        return render_template('models/create.html')
+    print('Method types', request.method)
+    return redirect(url_for('models.create'))
+    return render_template('models/create.html')
 
-        @bp.route('/<my_event>/comments', methods = ['GET', 'POST'])
-        @login_required
-        def comment (id):
-            form = CommentForm()
-            event_obj = website.query.get(id=id).first()
-            if form.validate_on_submit():
-                comment = Comment(body=form.body.data, website=event_obj,user=User)
-                db.session.add(comment)
-                db.session.commit()
-            
-                print('Comment added','success')
+    @bp.route('/<my_event>/comments', methods=['GET', 'POST'])
+    @login_required
+    def comment(id):
+        form = CommentForm()
+        event_obj = website.query.get(id=id).first()
+        if form.validate_on_submit():
+            comment = Comment(body=form.body.data,
+                              website=event_obj, user=User)
+            db.session.add(comment)
+            db.session.commit()
 
-                return redirect(url_for('models.show', id=event_obj.id))
-            return render_template('website/show.html', website=event_obj, form=form)
+            print('Comment added', 'success')
+
+            return redirect(url_for('models.show', id=event_obj.id))
+        return render_template('website/show.html', website=event_obj, form=form)
